7bcae0111544f9a879137a87d19de80a968bdb58
elbie-em <emoonga1993@gmail.com>
2020-05-21 17:38:12 +0200

    Run database  migrations

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,2 +1,7 @@
 class FriendRequest < ApplicationRecord
+  validates :requester_id, presence: true
+  validates :requestee_id, presence: true
+
+  belongs_to :requester, class_name: 'User', foreign_key: :requester_id
+  belongs_to :requestee, class_name: 'User', foreign_key: :requestee_id
 end
--- db/migrate/20200521152919_create_friend_requests.rb
+++ db/migrate/20200521152919_create_friend_requests.rb
@@ -1,6 +1,8 @@
 class CreateFriendRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :friend_requests do |t|
+      t.integer :requester_id
+      t.integer :requestee_id
 
       t.timestamps
     end
