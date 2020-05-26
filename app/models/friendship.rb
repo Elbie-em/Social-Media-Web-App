263130b1288cc7fe4c4b716949cf0f5393c70484
elbie-em <emoonga1993@gmail.com>
2020-05-26 17:13:11 +0200

    Friendship functionality associations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,7 +1,7 @@
 class Friendship < ApplicationRecord
   validates :status, presence: true
   validates :requestee, presence: true
-  validates :request, presence: true
+  validates :requester, presence: true
 
   belongs_to :requestee, class_name: 'User', foreign_key: :requestee_id
   belongs_to :requester, class_name: 'User', foreign_key: :requester_id
