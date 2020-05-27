b6bef200012c7d5eaede718adae39d83c5f0ab47
elbie-em <emoonga1993@gmail.com>
2020-05-27 19:24:40 +0200

    Testing FriendRequest Model validations, Complete

--- spec/models/friend_request_spec.rb
+++ spec/models/friend_request_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe FriendRequest, type: :model do
-  
+  context 'Checking for Validations on FriendRequest Model' do
+    let(:requester) { User.create!(name: 'john doe', email: 'johndoe@example.com', password: 'azerty012') }
+    let(:requestee) { User.create!(name: 'jane doe', email: 'janedoe@example.com', password: 'azerty012') }
+    let(:subject) { FriendRequest.create!(requester_id: requester.id, requestee_id: requestee.id) }
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a requester_id' do
+      subject.requester_id = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a requestee_id' do
+      subject.requestee_id = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
